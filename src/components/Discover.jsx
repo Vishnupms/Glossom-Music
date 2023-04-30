d4ed7edb255dddc2aa86aad3b107357d2082e060
codeblooded01 <vishnupms1998@gmail.com>
2023-04-30 12:26:42 +0530

    client side almost completed

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -27,6 +27,9 @@ function Discover() {
   },[])
 
   return (
+
+
+    
     <div className='flex flex-col'>
     <div className='w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10'>
       <h2 className='font-bold text-2xl text-white text-left'>Discover {genreTitle}</h2>
